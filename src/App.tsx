9c5054c29e27a8aa5975ccbfc8b74b25ef83e6fa
Evgeniy Klimovich <kliasanik@gmail.com>
2025-11-09 15:03:03 +0300

    ready for lesson 2

--- src/App.tsx
+++ src/App.tsx
@@ -14,15 +14,9 @@ export const App= () => {
       { id: 3, title: 'ReactJS', isDone: false },
    ]
 
-   const tasks2: Task[] = [
-      { id: 1, title: 'Hello world', isDone: true },
-      { id: 2, title: 'I am Happy', isDone: false },
-      { id: 3, title: 'Yo', isDone: false },
-   ]
   return (
       <div className="app">
         <TodolistItem title="What to learn" tasks ={tasks1}/>
-        <TodolistItem title="Man City" tasks ={tasks2}/>
       </div>
   )
 }
