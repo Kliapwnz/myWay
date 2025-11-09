681667cd9636357ae93e097bd0571278b8f4ecb8
Evgeniy Klimovich <kliasanik@gmail.com>
2025-11-09 14:15:25 +0300

    create 2 arrays and use this with props

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,28 @@
 import './App.css'
 import {TodolistItem} from "./TodolistItem";
 
+export type Task = {
+   id:number,
+   title:string,
+   isDone:boolean
+
+}
 export const App= () => {
+   const tasks1: Task[] = [
+      { id: 1, title: 'HTML&CSS', isDone: true },
+      { id: 2, title: 'JS', isDone: true },
+      { id: 3, title: 'ReactJS', isDone: false },
+   ]
+
+   const tasks2: Task[] = [
+      { id: 1, title: 'Hello world', isDone: true },
+      { id: 2, title: 'I am Happy', isDone: false },
+      { id: 3, title: 'Yo', isDone: false },
+   ]
   return (
       <div className="app">
-        <TodolistItem title="What to learn"/>
-        <TodolistItem title="Man City"/>
+        <TodolistItem title="What to learn" tasks ={tasks1}/>
+        <TodolistItem title="Man City" tasks ={tasks2}/>
       </div>
   )
 }
