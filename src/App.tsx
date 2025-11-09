bbde195307150dc2dac4d1d50bbf1e580e36ea6f
Evgeniy Klimovich <kliasanik@gmail.com>
2025-11-09 13:53:04 +0300

    aaa

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import './App.css'
+import {TodolistItem} from "./TodolistItem";
+
+function App() {
+  return (
+      <div className="app">
+        <TodolistItem/>
+      </div>
+  )
+}
+
+export default App
