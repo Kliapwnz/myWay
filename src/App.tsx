4fd4842be2dde511108c328a17fbaf25485aeee3
Evgeniy Klimovich <kliasanik@gmail.com>
2025-11-09 15:12:27 +0300

    create functions for delete tasks and use props

--- src/App.tsx
+++ src/App.tsx
@@ -2,23 +2,32 @@ import './App.css'
 import {TodolistItem} from "./TodolistItem";
 
 export type Task = {
-   id:number,
-   title:string,
-   isDone:boolean
+   id: number,
+   title: string,
+   isDone: boolean
 
 }
-export const App= () => {
-   const tasks1: Task[] = [
-      { id: 1, title: 'HTML&CSS', isDone: true },
-      { id: 2, title: 'JS', isDone: true },
-      { id: 3, title: 'ReactJS', isDone: false },
+export const App = () => {
+   let tasks: Task[] = [
+      {id: 1, title: 'HTML&CSS', isDone: true},
+      {id: 2, title: 'JS', isDone: true},
+      {id: 3, title: 'ReactJS', isDone: false},
    ]
+   const deleteTask = (taskId: number) => {
+      tasks = tasks.filter(task => {
+         return task.id !== taskId
+      })
+      console.log(tasks)
+   }
 
-  return (
+   return (
       <div className="app">
-        <TodolistItem title="What to learn" tasks ={tasks1}/>
+         <TodolistItem title="What to learn"
+                       tasks={tasks}
+                       deleteTask={deleteTask}
+         />
       </div>
-  )
+   )
 }
 
 
