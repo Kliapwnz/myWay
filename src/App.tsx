72d62934f3fcc08cbf6b8fffc76eb2d0be3e9b72
Evgeniy Klimovich <kliasanik@gmail.com>
2025-11-09 15:25:31 +0300

    add useState for filter tasks

--- src/App.tsx
+++ src/App.tsx
@@ -10,6 +10,7 @@ export type Task = {
 
 }
 export const App = () => {
+   const [filter, setFilter] = useState("all")
    const [tasks, setTasks] = useState<Task[]>([
       {id: 1, title: 'HTML&CSS', isDone: true},
       {id: 2, title: 'JS', isDone: true},
@@ -21,11 +22,18 @@ export const App = () => {
       })
       setTasks(filteredTasks)
    }
+   let filteredTasks = tasks
+   if (filter === 'active') {
+      filteredTasks = tasks.filter(task => !task.isDone)
+   }
+   if (filter === 'completed') {
+      filteredTasks = tasks.filter(task => task.isDone)
+   }
 
    return (
       <div className="app">
          <TodolistItem title="What to learn"
-                       tasks={tasks}
+                       tasks={filteredTasks}
                        deleteTask={deleteTask}
          />
       </div>
