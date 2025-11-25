abbefcdedd7497acc8f5c8549209fd9d4da06cc9
Evgeniy Klimovich <kliasanik@gmail.com>
2025-11-25 18:55:33 +0300

    deleteTodolistAC create and delete action

--- src/NavButton.ts
+++ src/NavButton.ts
@@ -1,11 +1,11 @@
-import { styled } from '@mui/material/styles'
+import {styled} from '@mui/material/styles'
 import Button from '@mui/material/Button'
 
 type Props = {
    background?: string
 }
 
-export const NavButton = styled(Button)<Props>(({ background }) => ({
+export const NavButton = styled(Button)<Props>(({background}) => ({
    minWidth: '110px',
    fontWeight: 'bold',
    boxShadow: '0 0 0 2px #054B62, 4px 4px 0 0 #054B62',
