90d0393c430c95bd9c198dd55f6879f399709872
Evgeniy Klimovich <kliasanik@gmail.com>
2025-11-25 18:55:24 +0300

    deleteTodolistAC create and delete action

--- src/model/todolists-reducer.ts
+++ src/model/todolists-reducer.ts
@@ -3,13 +3,13 @@ import type {Todolist} from '../App'
 const initialState: Todolist[] = []
 
 export const todolistsReducer = (state: Todolist[] = initialState, action: Actions): Todolist[] => {
-switch (action.type) {
-   case "delete_todolist" : {
-      return state.filter(todolist => todolist.id !== action.payload.id)
+   switch (action.type) {
+      case "delete_todolist" : {
+         return state.filter(todolist => todolist.id !== action.payload.id)
+      }
+      default:
+         return state
    }
-   default:
-      return state
-}
 }
 
 export type DeleteTodolistAction = {
@@ -19,7 +19,7 @@ export type DeleteTodolistAction = {
    }
 }
 export const deleteTodolistAC = (id: string): DeleteTodolistAction => {
-   return {type: 'delete_todolist', payload: { id }} as const
+   return {type: 'delete_todolist', payload: {id}} as const
 }
 
 type Actions = DeleteTodolistAction
