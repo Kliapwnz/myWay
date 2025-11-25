92c36dc7b7a8739d7ae56afabac1e06944f06c38
Evgeniy Klimovich <kliasanik@gmail.com>
2025-11-25 15:38:15 +0300

    write first reducer

---
+++ src/model/todolists-reducer.ts
@@ -0,0 +1,17 @@
+import type {Todolist} from '../App'
+
+const initialState: Todolist[] = []
+
+export const todolistsReducer = (state: Todolist[] = initialState, action: Actions): Todolist[] => {
+switch (action.type) {
+   case "delete_todolist" : {
+      return state
+   }
+   default:
+      return state
+}
+}
+type Actions = {
+   type: string
+   payload: any
+}
