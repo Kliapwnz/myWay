fdb29375cac1310a8f9655eae1259ff11444f6f3
Evgeniy Klimovich <kliasanik@gmail.com>
2025-11-27 23:17:48 +0300

    start reducer add todolist

--- src/model/todilists-reducer.test.ts
+++ src/model/todilists-reducer.test.ts
@@ -20,3 +20,19 @@ test('correct todolist should be deleted', () => {
    expect(endState.length).toBe(1)
    expect(endState[0].id).toBe(todolistId2)
 })
+
+test('correct todolist should be created', () => {
+   const todolistId1 = v1()
+   const todolistId2 = v1()
+
+   const startState: Todolist[] = [
+      {id: todolistId1, title: 'What to learn', filter: 'all'},
+      {id: todolistId2, title: 'What to buy', filter: 'all'},
+   ]
+
+   const title = 'New todolist'
+   const endState = todolistsReducer(startState, createTodolistAC(title))
+
+   expect(endState.length).toBe(3)
+   expect(endState[2].title).toBe(title)
+})
