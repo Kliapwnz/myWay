7bf1633cbdec84434afccdc3e68cd1171a43d87f
Evgeniy Klimovich <kliasanik@gmail.com>
2025-11-27 22:43:24 +0300

    little changes

--- src/model/todilists-reducer.test.ts
+++ src/model/todilists-reducer.test.ts
@@ -16,9 +16,7 @@ test('correct todolist should be deleted', () => {
 
    const endState = todolistsReducer(startState, deleteTodolistAC(todolistId1))
 
-   // 3. Проверка, что действие измененило state соответствующим образом
-   // в массиве останется один тудулист
+
    expect(endState.length).toBe(1)
-   // удалится нужный тудулист, не любой
    expect(endState[0].id).toBe(todolistId2)
 })
