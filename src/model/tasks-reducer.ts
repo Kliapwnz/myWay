c1abbc0299d278ab7a308e8f34ba2b336eeaa0dd
Evgeniy Klimovich <kliasanik@gmail.com>
2025-11-29 21:04:10 +0300

    delete task done

--- src/model/tasks-reducer.ts
+++ src/model/tasks-reducer.ts
@@ -6,16 +6,25 @@ const initialState: TasksState = {}
 export const tasksReducer = (state: TasksState = initialState, action: Actions): TasksState => {
    switch (action.type) {
       case 'add_todolist': {
-         return {...state, [action.payload.id]:[]}
+         return {...state, [action.payload.id]: []}
       }
       case "delete_todolist": {
          const newState = {...state}
          delete newState[action.payload.id]
          return newState
       }
+      case "delete_task" : {
+         return {...state,
+            [action.payload.todolistId]: state[action.payload.todolistId].filter(task => task.id !== action.payload.taskId)}
+      }
       default:
          return state
    }
+}
+export type deleteTaskType = ReturnType<typeof deleteTaskAC>
+export const deleteTaskAC = (todolistId: string, taskId: string): deleteTaskType => {
+   return {type: "delete_task", payload: {todolistId, taskId}} as const
+
 }
 
 type Actions = addTodolistType | DeleteTodolistActionType
